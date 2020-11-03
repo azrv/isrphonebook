3950dc0735a3f835120ef7d64bccbfe6532ce19d
greenwookez <greenwookez@gmail.com>
2020-11-03 19:18:31 +0300

    App Class added

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to stadsdsdsrt working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  state = {
+    i: 0
+  }
+
+  onPress = () => {
+    this.setState({ i: this.state.i + 1 })
+  }
+
+  render() {
+    return (
+      <View style={styles.wrapper}>
+        <View style={styles.container}>
+          <Text>Search</Text>
+        </View>
+
+        <View style={styles.container}>
+          <TouchableOpacity
+           style={styles.button}
+           onPress={this.onPress}
+          >
+            <Text>Tap</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.container}>
+          <Text>You've tapped {this.state.i} times.</Text>
+        </View>
+
+        <StatusBar style="auto" />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {
     flex: 1,
+    padding: 15,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  container: {
+    marginTop: 45,
+    padding: 15,
+    alignItems: "center",
+    height: 65,
+    backgroundColor: '#6c6c6c',
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#DDDDDD',
+    padding: 5,
+    width: 70,
+    height: 40,
+    textWeight: в
   },
 });
+
+export default App;
