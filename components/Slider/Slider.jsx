adde68132b89ee9d61e639c349348148c6fe7d1a
greenwookez <greenwookez@gmail.com>
2020-11-03 22:40:11 +0300

    Slider styles added

--- components/Slider/Slider.jsx
+++ components/Slider/Slider.jsx
@@ -4,6 +4,39 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
 export default Slider = () => {
   return (
-    <Text>I am slider.</Text>
+    <View style={sliderStyles.sliderContainer}>
+      <View style={sliderStyles.sliderCard}>
+        <View style={sliderStyles.cardImage}>
+          <Text>Card Image</Text>
+        </View>
+        <Text style={sliderStyles.cardTitle}>Card Title</Text>
+      </View>
+      <View style={sliderStyles.sliderCard}>
+        <View style={sliderStyles.cardImage}>
+          <Text>Card Image2</Text>
+        </View>
+        <Text style={sliderStyles.cardTitle}>Card Title2</Text>
+      </View>
+    </View>
   )
 };
+
+const sliderStyles = StyleSheet.create({
+  sliderContainer: {
+    marginTop: 20,
+    backgroundColor:'#6c6c6c',
+  },
+  sliderCard: {
+    backgroundColor:'#ddd',
+    height: 250,
+  },
+  cardImage: {
+    backgroundColor:'red',
+    height:200,
+  },
+  cardTitle: {
+    backgroundColor:'#6c6c6c',
+    textAlign: 'center',
+    height:50,
+  },
+});
